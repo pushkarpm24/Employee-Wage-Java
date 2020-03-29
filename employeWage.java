f262ae5b319d64442f4d313d2c3bd4f73fe71936
pushkarpm24 <pushkarmorey555@gmail.com>
2020-03-30 00:17:30 +0530

    ADDED:code to get wage of emp till working day riched

--- employeWage.java
+++ employeWage.java
@@ -5,14 +5,16 @@ public class employeWage {
 	public static final int IS_PART_TIME = 1;
 	public static final int IS_FULL_TIME = 2;
 	public static final int Emp_Rate_Per_Hr = 20;
-	public static final int NUM_OF_WORKING_DAYS=2;
+	public static final int NUM_OF_WORKING_DAYS = 2;
+	public static final int MAX_HRS_IN_MONTH = 10;
 
 	public static void main( String[] args) {
 	//Variables
-	int empHrs = 0,empWage = 0,totalEmpWage=0;
+	int empHrs = 0,totalEmpHrs = 0,totalWorkingDays = 0;
 	//Computation
-	for (int day = 0; day < NUM_OF_WORKING_DAYS; day++)
+	while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS)
 {
+	totalWorkingDays++;
 	int empCheck = (int) Math.floor(Math.random() * 10) % 3;
 	switch (empCheck)
 	{
@@ -26,9 +28,10 @@ public class employeWage {
 				empHrs = 0;
 	}
 
-		empWage = empHrs * Emp_Rate_Per_Hr;
-		totalEmpWage += empWage;
+		totalEmpHrs += empHrs;
+		System.out.println("Day#" + totalWorkingDays + "Emp hrs" + empHrs);
 }
+		int totalEmpWage = totalEmpHrs * Emp_Rate_Per_Hr;
 		System.out.println("Total Emp Wage:" + totalEmpWage);
 
 
