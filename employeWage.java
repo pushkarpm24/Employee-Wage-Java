dde29948d18e940d7e3722cec3c9b393197f4799
pushkarpm24 <pushkarmorey555@gmail.com>
2020-03-29 20:49:53 +0530

    ADDED:code to find emp wage using switch

--- employeWage.java
+++ employeWage.java
@@ -2,22 +2,27 @@
 
 public class employeWage {
 
+	public static final int IS_PART_TIME = 1;
+	public static final int IS_FULL_TIME = 2;
+	public static final int Emp_Rate_Per_Hr = 20;
+
 	public static void main( String[] args) {
-	//CONSTANTS
-	int IS_PART_TIME = 1;
-	int IS_FULL_TIME = 2;
-	int Emp_Rate_Per_Hr = 20;
 	//Variables
 	int empHrs = 0;
 	int empWage = 0;
 	//Computation
-	double empCheck = Math.floor(Math.random() * 10) % 3;
-		if (empCheck == IS_FULL_TIME)
-			empHrs = 8;
-		else if (empCheck == IS_PART_TIME)
-			empHrs = 4;
-		else
-			empHrs = 0;
+	int empCheck = (int) Math.floor(Math.random() * 10) % 3;
+	switch (empCheck) {
+
+		case IS_FULL_TIME:
+				empHrs = 8;
+				break;
+		case IS_PART_TIME:
+				empHrs = 4;
+				break;
+		default:
+				empHrs = 0;
+}
 
 		empWage = empHrs * Emp_Rate_Per_Hr;
 		System.out.println("Emp Wage:" + empWage);
