46b74cbcb7841d61d12219e1aa2cf2255a825cd3
pushkarpm24 <pushkarmorey555@gmail.com>
2020-03-29 22:09:06 +0530

    ADDED:code to calculate wages for a month

--- employeWage.java
+++ employeWage.java
@@ -5,15 +5,17 @@ public class employeWage {
 	public static final int IS_PART_TIME = 1;
 	public static final int IS_FULL_TIME = 2;
 	public static final int Emp_Rate_Per_Hr = 20;
+	public static final int NUM_OF_WORKING_DAYS=2;
 
 	public static void main( String[] args) {
 	//Variables
-	int empHrs = 0;
-	int empWage = 0;
+	int empHrs = 0,empWage = 0,totalEmpWage=0;
 	//Computation
+	for (int day = 0; day < NUM_OF_WORKING_DAYS; day++)
+{
 	int empCheck = (int) Math.floor(Math.random() * 10) % 3;
-	switch (empCheck) {
-
+	switch (empCheck)
+	{
 		case IS_FULL_TIME:
 				empHrs = 8;
 				break;
@@ -22,10 +24,13 @@ public class employeWage {
 				break;
 		default:
 				empHrs = 0;
-}
+	}
 
 		empWage = empHrs * Emp_Rate_Per_Hr;
-		System.out.println("Emp Wage:" + empWage);
+		totalEmpWage += empWage;
+}
+		System.out.println("Total Emp Wage:" + totalEmpWage);
+
 
 }
 
