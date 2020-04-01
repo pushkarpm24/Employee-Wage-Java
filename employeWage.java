707aaca65d94be8f73d91ab3034dc8b53fe395c7
pushkarpm24 <pushkarmorey555@gmail.com>
2020-04-01 08:49:28 +0530

    ADDED:code to manage wage using interface

--- employeWage.java
+++ employeWage.java
@@ -1,53 +1,49 @@
+interface WageMethod	{
 
-public class employeWage	{
+	public void computeTotalEmpWage( CompanyEmpWage obj );
 
+}
+
+public class employeWage implements WageMethod	{
+	//CONSTANTS
 	public final int IS_PART_TIME = 1;
 	public final int IS_FULL_TIME = 2;
-
-
-	public void totEmpWageComputation( CompanyEmpWage companyEmployee )	{
-   	//Variables
-   	int empHrs = 0,totalEmpHrs = 0,totalWorkingDays = 0;
-   		//Computation
-   		while ( totalEmpHrs <= companyEmployee.getMaxHrsInMonth() &&
-				totalWorkingDays < companyEmployee.getMaxNumOfWorkingDays())	{
-   				totalWorkingDays++;
-   				int empCheck = (int) Math.floor(Math.random() * 10) % 3;
-   					switch (empCheck)
-   					{
-      						case IS_FULL_TIME:
-            							empHrs = 8;
-            							break;
-      						case IS_PART_TIME:
-            							empHrs = 4;
-            							break;
-      						default:
-            							empHrs = 0;
-   					}
-
-      						totalEmpHrs += empHrs;
-      						System.out.println("Day#" + totalWorkingDays + "Emp hrs" + empHrs);
+	//COMPUTATION
+	public void computeTotalEmpWage( CompanyEmpWage companyEmployee )	{
+		int empHrs = 0, totalEmpHrs = 0, totalWorkingDays = 0;
+		while( totalEmpHrs <= companyEmployee.getMaxHrsInMonth() &&
+				totalWorkingDays < companyEmployee.getMaxNumOfWorkingDays() )  {
+			totalWorkingDays++;
+			int empCheck = (int) ( Math.random() * 10 ) % 3;
+			switch (empCheck) {
+				case IS_PART_TIME:
+					empHrs = 4;
+					break;
+				case IS_FULL_TIME:
+					empHrs = 8;
+					break;
+				default:
+					empHrs = 0;
 			}
-								companyEmployee.setTotalEmpWage( totalEmpHrs * companyEmployee.getEmpRatePerHour() );
-								System.out.println("Total Wage of " + companyEmployee.getCompanyName() + " Employee is " + companyEmployee.getTotalEmpWage());
-
+			totalEmpHrs += empHrs;
+			System.out.println("Day#: " + totalWorkingDays + " Emp Hr: " +  empHrs);
+		}
+		companyEmployee.setTotalEmpWage( totalEmpHrs * companyEmployee.getEmpRatePerHour() );
+		System.out.println("Total Wage of " + companyEmployee.getCompanyName() + " Employee is " + companyEmployee.getTotalEmpWage());
 	}
 
-	public static void main (String[] args) {
-		//CREATING OBJECTS
+	//MAIN
+	public static void main(String args[]) {
 		employeWage wage = new employeWage();
 		//DECLARING ARRAY OF COMPANY EMPLOYEE WAGE OBJECT
 		CompanyEmpWage[] company = new CompanyEmpWage[2];
 
 		company[0] = new CompanyEmpWage( "BRIDGELABZ", 20, 20, 100 );
-		wage.totEmpWageComputation(company[0]);
+		wage.computeTotalEmpWage(company[0]);
 
-		company[1] = new CompanyEmpWage( "GOOGLE", 30, 20, 70 );
-		wage.totEmpWageComputation(company[1]);
-
+		company[1] = new CompanyEmpWage( "CAPGEMENI", 30, 20, 70 );
+		wage.computeTotalEmpWage(company[1]);
 	}
-
-
 }
 
 class CompanyEmpWage	{
