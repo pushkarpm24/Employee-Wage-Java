865e5e08a387026c8b11f7d19253c8e171128415
pushkarpm24 <pushkarmorey555@gmail.com>
2020-04-01 09:52:02 +0530

    ADDED:code to get emp wage when company queried

--- employeWage.java
+++ employeWage.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 
 interface WageMethod	{
 
@@ -53,6 +54,29 @@ public class employeWage implements WageMethod	{
 
 		company.add( new CompanyEmpWage( "GOOGLE", 30, 20, 70 ));
 		wage.computeTotalEmpWage(company.get(1));
+
+		while( true )	{
+			System.out.println("Welcome to Employee wage portal");
+			System.out.println("Enter your choice to see the Total wage of an Employee of a particular company");
+			System.out.println("Press 1 for Bridgelabz");
+			System.out.println("Press 2 for Google");
+			System.out.println("Press 3 to Exit");
+			Scanner scanner = new Scanner(System.in);
+			int option = scanner.nextInt();
+			if( option == 1 )	{
+				System.out.println( "The Total Employee Wage of Bridgelabz is: " + company.get(0).getTotalEmpWage() );
+			}
+			else if( option == 2 )	{
+				System.out.println( "The Total Employee Wage of Google is: " + company.get(1).getTotalEmpWage() );
+			}
+			else if( option == 3 )	{
+				System.exit(0);
+			}
+			else	{
+				System.out.println("Invalid option \n Try again");
+			}
+			System.out.println();
+		}
 	}
 }
 
